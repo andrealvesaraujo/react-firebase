017164874279e76503069993fbcbe230b679fa02
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-07-19 18:39:34 -0300

    Usando nossa database

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
+import {useState} from 'react'
+import './style.css'
 import firebase from './firebaseConnection'
 
 function App() {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd(){
+    await firebase.firestore().collection('posts')
+    .add({
+      titulo: titulo,
+      autor: autor
+    })
+    .then(()=>{
+      console.log('Dados cadatrados com Sucesso')
+      setTitulo('')
+      setAutor('')
+    })
+    .catch((error)=>{
+      console.log('Gerou algum erro: ' + error)
+    })
+  }
+
+  async function buscaPost(){
+    await firebase.firestore().collection('posts')
+    .doc('123')
+    .get()
+    .then((snapshot)=>{
+      setTitulo(snapshot.data().titulo)
+      setAutor(snapshot.data().autor)
+    })
+    .catch((error)=>{
+      console.log('Gerou algum erro: ' + error)
+    })
+  }
+
   return (
     <div className="App">
-      <h1>React JS + Firebase :( </h1>
+      <h1>React JS + Firebase </h1> <br/>
+
+      <div className="container">
+
+        <label>Titulo: </label>
+        <textarea type="text" value={titulo} onChange={(e)=> setTitulo(e.target.value)} />
+
+        <label>Autor: </label>
+        <textarea type="text" value={autor} onChange={(e)=> setAutor(e.target.value)} />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+
+        <button onClick={buscaPost}>Buscar Post</button>
+
+
+      </div>
+
     </div>
   );
 }
