a0ad530213007b9e4fcc1d878e4c654730c6055b
Andre Alves <andrealvesaraujo.1995@gmail.com>
2022-10-22 21:31:29 -0300

    Criando página home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,52 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
 export default function Home(){
-    return (
-      <div>
-        <h1>Pagina Home</h1>
-      </div>
-    )
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleLogin(e){
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      alert("Teste")
+    } else {
+      alert('Preencha todos os campos')
+    }
+    
   }
+
+  return (
+    <div className='home-container'>
+      <h1>Lista de tarefas</h1>
+      <span>Gerencia sua agenda de forma fácil</span>
+
+      <form className='form' onSubmit={handleLogin}>
+        <input 
+          type="text" 
+          placeholder='Digite seu email...' 
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}  
+        />
+
+        <input 
+          autoComplete={false}
+          type="password" 
+          placeholder='******' 
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}  
+        />
+
+        <button type='submit'>Acessar</button>
+
+      </form>
+
+      <Link to="/register" className='button-link'>
+        Não possui uma conta? Cadastre-se
+      </Link> 
+
+
+    </div>
+  )
+}
